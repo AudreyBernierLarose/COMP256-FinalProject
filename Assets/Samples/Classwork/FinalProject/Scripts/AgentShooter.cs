264118c6d7bc3e76f3b594c2a7f46477accded1d
AudreyBernierLarose <abernier@my.centennialcollege.ca>
2024-04-02 20:46:33 -0400

    FEAT: Added assets to decorate the environment as well as the training area.

--- Assets/Samples/Classwork/FinalProject/Scripts/AgentShooter.cs
+++ Assets/Samples/Classwork/FinalProject/Scripts/AgentShooter.cs
@@ -5,20 +5,20 @@ using Unity.MLAgents.Actuators;
 
 public class AgentShooter : Agent
 {
-    Rigidbody rb;
+    private Rigidbody rb;
+    private float x_min = -35f;
+    private float x_max = 35f;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
     }
 
-    public Transform Target;
-    float x_min = -35f;
-    float x_max = 35f;
+    public Transform Target;    
 
     public override void OnEpisodeBegin()
     {
-        Target.localPosition = new(Random.Range(-35f, 35f), 0.5f, -40f);
+        Target.localPosition = new(Random.Range(-35f, 35f), 10f, -40f);
 
         Vector3 initialPosition = transform.localPosition;
         if (initialPosition.x < x_min || initialPosition.x > x_max)
@@ -36,29 +36,27 @@ public class AgentShooter : Agent
         sensor.AddObservation(rb.velocity.x);
     }
 
-    public float forceMultiplier = 15;
+    public float force_multiplier = 15;
 
     public override void OnActionReceived(ActionBuffers actionBuffers)
     {
-        Vector3 directionToTarget = Target.localPosition - transform.localPosition;
-        directionToTarget.y = 0f; 
-        directionToTarget.z = 0f;
-        directionToTarget.Normalize();
+        Vector3 direction_to_target = Target.localPosition - transform.localPosition;
+        direction_to_target.y = 0f; 
+        direction_to_target.z = 0f;
+        direction_to_target.Normalize();
 
-        Vector3 controlSignal = Vector3.zero;
-        controlSignal.x = directionToTarget.x;
-        rb.AddForce(controlSignal * forceMultiplier);
+        Vector3 control_signal = Vector3.zero;
+        control_signal.x = direction_to_target.x;
+        rb.AddForce(control_signal * force_multiplier);
 
         float x_difference = Mathf.Abs(transform.localPosition.x - Target.localPosition.x);
-        if (x_difference < 0.1f)
-        {
+        if (x_difference < 0.1f) {
             SetReward(1.0f);
             EndEpisode();
         }
-        else if (transform.localPosition.x < x_min || transform.localPosition.x > x_max)
-        {
+        else if (transform.localPosition.x < x_min || transform.localPosition.x > x_max)        
             EndEpisode();
-        }
+        
     }
 
     public override void Heuristic(in ActionBuffers actionsOut)
